e7fd0c8920ee0801d5c1a8772cc1350a92a862e5
keerky172 <kevincamilomoramonroy@gmail.com>
2026-03-16 14:22:23 -0500

    Resolver conflicto y mantener implementacion

--- frontend/src/components/Inicio.jsx
+++ frontend/src/components/Inicio.jsx
@@ -5,19 +5,7 @@ import './Inicio.css';
 function Inicio() {
   return (
     <>
-      <header className="header">
-        <Link to="/login" className="header-link">Iniciar sesión</Link>
-      </header>
-
-      <div className="content">
-        <h1>Bienvenido a Nuestra Empresa</h1>
-        <p>Aquí va la información de la empresa, sus servicios, misión, visión, etc.</p>
-        <Link to="/registro" className="registro-link">Regístrate</Link>
-        <br /><br />
-        <Link to="/home" style={{ color: '#007bff', textDecoration: 'underline' }}>
-          Ver módulo de orientación sin login
-        </Link>
-      </div>
+  
     </>
   );
 }
